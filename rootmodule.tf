440a5bc7cc47fdf818df3ceb2cadfc4e98e87abd
prabala <radhakrishnaprabala8@gmail.com>
2024-10-02 18:45:20 +0530

    rootmodule.tf vr2

--- rootmodule.tf
+++ rootmodule.tf
@@ -11,6 +11,8 @@ module "vpc" {
 
 module "web" {
   source = "git::https://github.com/prabalark/tf-module-app.git"
+
+  for_each = var.app
   instance_type = each.value["instance_type"]
   subnet_ids = element(lookup(lookup(lookup(lookup(module.vpc, "main" , null),"subnets",null),each.value["subnet_name"],null)
       ,"subnet_ids",null ),0)

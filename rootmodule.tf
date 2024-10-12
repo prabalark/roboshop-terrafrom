c8944fab6e265d3f81acf99a5a1a57556305015e
prabala <radhakrishnaprabala8@gmail.com>
2024-10-12 17:32:31 +0530

    rootmodule.tf vr1

--- rootmodule.tf
+++ rootmodule.tf
@@ -43,15 +43,12 @@ module "docdb" {
   kms_arn = var.kms_arn
 
   subnet_id = lookup(lookup(lookup(lookup(module.vpc,"main",null),"subnets",null),each.value["subnet_name"],null),"subnet_ids",null )
-  vpc_id = lookup(lookup(module.vpc,"main",null),"vpc_id",null)
+  # giving in local.tf : vpc_id = lookup(lookup(module.vpc,"main",null),"vpc_id",null)
   allow_app_cidr = lookup(lookup(lookup(lookup(module.vpc,"main",null),"subnets",null),each.value["allow_db_cidr"],null),"subnet_cidrs",null )
 }
 
 
-variable "vpc_id" {}
-variable "allow_db_cidr" {}
-
 variable "kms_key_id" {}
-variable "subnets" {}
+
 
 

620744a775a9fa874f028140feb104dda052d57f
prabala <radhakrishnaprabala8@gmail.com>
2024-10-19 12:16:08 +0530

    rootmodule.tf

--- rootmodule.tf
+++ rootmodule.tf
@@ -118,17 +118,6 @@ module "alb" {
 }
 
 
-variable "name" {}
-variable "env" {}
-variable "vpc_id" {}
-variable "allow_alb_cidr" {}
-
-variable "internal" {}
-variable "subnets" {}
-
-variable "tags" {}
-
-
 
 
 

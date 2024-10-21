300ba746357a5d44bfdbeed8049038af7530eb16
prabala <radhakrishnaprabala8@gmail.com>
2024-10-21 23:19:09 +0530

    rootmodule.tf

--- rootmodule.tf
+++ rootmodule.tf
@@ -37,6 +37,8 @@ module "web" { # app
 
   lb_dns_name = lookup(lookup(module.alb, each.value["lb_type"], null), "dns_name1", null) #tf-laodbal-output.tf
 
+  kms_arn      =
+
   tags             = merge(local.tags, { Monitor = "true" })
   env              = var.env
   bastion_cidr     = var.bastion_cidr

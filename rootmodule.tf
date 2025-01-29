82f2031acd919fd7fc288449e12fa5b6a85bd3b3
prabala <prabalaradhakrishna300@gmail.com>
2025-01-29 22:27:44 +0530

    rootmodule.tf

--- rootmodule.tf
+++ rootmodule.tf
@@ -139,6 +139,17 @@ module "alb" {
 
 }
 
+module "eks" {
+  source             = "git::https://github.com/prabalark/tf-module-eks.git"
+  ENV                = var.env
+  eks_version        = 1.27
+  PRIVATE_SUBNET_IDS = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "app", null), "subnet_ids", null)
+  PUBLIC_SUBNET_IDS  = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "public", null), "subnet_ids", null)
+  DESIRED_SIZE       = 2
+  MAX_SIZE           = 2
+  MIN_SIZE           = 2
+  kms_arn            = var.kms_arn
+}
 
 #### Load Runner
 data "aws_ami" "ami" {
